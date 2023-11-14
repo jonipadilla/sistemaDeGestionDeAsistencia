2ff6c4bcc21e355440f8810d99d8d263d466ed3f
jonatan padilla <padilla.joni@gmail.com>
2023-11-14 15:46:59 -0300

    primer commit

---
+++ altausuario.js
@@ -0,0 +1,22 @@
+let nombre;
+let dni;
+
+
+
+do {
+    nombre = prompt('Ingrese el nombre');
+    // edad = parseInt(prompt('Ingrese Su Edad'));
+    // dni = parseInt(prompt('Ingrese Su Numero de Dni'));
+    
+} while (nombre === null || nombre === "");
+
+alert();
+
+// console.log("Has ingresado:", "Nombre:", nombre);
+
+// if (nombre === 2) {
+//     console.log('el resultado es:',nombre);
+// }
+// else{
+//     console.log('Seleccione cancelar para salir');
+// }
