bd0409d303e44945e8506a18b0bb03d270e40f54
jonatan padilla <padilla.joni@gmail.com>
2023-11-30 23:03:10 -0300

    agregado clase registrar usuario

---
+++ usuario.js
@@ -0,0 +1,75 @@
+class Usuario{
+    nombre;
+    apellido;
+    dni;
+    edad;
+    genero;
+
+    constructor(nombre,apellido,dni,edad,genero){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.genero = genero;
+    }
+}
+
+
+    
+let opcion = 0;
+let cantidadusuario = 0;
+opcion = parseInt(prompt("Seleccione 1 para registrar un nuevo usuario ó 2 para salir:"));
+cantidadusuario = parseInt(prompt("Ingrese la cantidad de usuarios a registrar:"));
+
+if (opcion !== 2){
+
+    const listaUsuarios = [];
+
+    function agregarUsuario(nombre, apellido, dni, edad, genero) {
+        const nuevoUsuario = new Usuario(nombre, apellido, dni, edad, genero);
+        listaUsuarios.push(nuevoUsuario);
+    }
+
+
+    for (let i = 0; i < cantidadusuario; i++) {
+  
+        agregarUsuario(
+            Usuario.nombre = prompt("ingrese Nombre"),
+            Usuario.apellido = prompt("Ingrese Apellido"),
+            Usuario.dni = prompt("Ingrese Dni"),
+            Usuario.edad = prompt("Ingrese Edad"),
+            Usuario.genero = prompt("Ingrese Genero")
+        );
+
+    }
+    
+    listaUsuarios.forEach(element => {
+        console.log(`Nombre: ${element.nombre}, Apellido: ${element.apellido}, DNI: ${element.dni}, Edad: ${element.edad}, Genero: ${element.genero}`);
+    });
+
+
+    //Busqueda
+
+    function buscarUsuariosPorNombre(nombreBuscado) {
+        const usuariosEncontrados = listaUsuarios.filter(
+          usuario => usuario.nombre.toLowerCase() === nombreBuscado.toLowerCase()
+        );
+      
+        return usuariosEncontrados;
+      }
+      
+      let nombreABuscar = prompt("ingrese el nombre a buscar");
+      const usuariosEncontrados = buscarUsuariosPorNombre(nombreABuscar);
+      
+      if (usuariosEncontrados.length > 0) {
+        console.log(`Usuarios encontrados con el nombre ${nombreABuscar}:`);
+        usuariosEncontrados.forEach(element => {
+          console.log(`Nombre: ${element.nombre}, Apellido: ${element.apellido}, DNI: ${element.dni}, Edad: ${element.edad}, Genero: ${element.genero}`);
+        });
+      } else {
+        console.log(`No se encontraron usuarios con el nombre ${nombreABuscar}.`);
+      }
+
+}
+    
+    
